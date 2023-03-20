b873ec21a98c83ae28c42db345cd08f016618647
Pablo Alastra <alastra.pablo@gmail.com>
2023-03-20 14:58:53 -0300

    more js

--- index.js
+++ index.js
@@ -44,3 +44,46 @@ $('input').after('<hr></hr>')
 
 // $('button').remove()
 
+$('button').on('click', function (e) {
+    $('button').hide(e)
+
+    setTimeout(() => {
+        $('button').show(e)
+    }, 1000);
+})
+
+$('button').click(function (e) {
+    $('h2').toggle(e)
+})
+
+$('button').on('click', function (e) {
+    $('h1').fadeOut(e)
+
+    setTimeout(() => {
+        $('h1').fadeIn(e)
+    }, 1000);
+})
+
+// $('h1').click(function (e) {
+//     $('h1').fadeToggle(e)
+// })
+
+// $('hr').on('click', function (e) {
+//     $('div').slideUp(e)
+
+//     setTimeout(() => {
+//         $('div').slideDown(e)
+//     }, 1000);
+// })
+
+// $('hr').click(function (e) {
+//     $('div').slideToggle(e)
+// })
+
+// $('hr').click(function () {
+//     $('h3').animate({ opacity: 0.5 })
+// })
+
+$('hr').on('click', function (e) {
+    $('div').slideUp(e).slideDown(e).animate({ opacity: 0.5 })
+})
